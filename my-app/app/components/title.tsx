b819184a2716ac7bc9b00d358781ecbb0a543499
abhijit <kamathabhijit2002@gmail.com>
2024-12-22 16:20:24 -0500

    added command text area and the title

---
+++ my-app/app/components/title.tsx
@@ -0,0 +1,11 @@
+export default function Title() {
+  return (
+    <div className="pt-3 inline-flex flex-row items-center gap-2">
+      <h1 className="text-purple-400 font-semibold text-xl">Learn Git</h1>
+      <p className="text-purple-200">| </p>
+      <p className="text-purple-200">
+        A playground to learn Git commands and to get familiar with commands
+      </p>
+    </div>
+  );
+}
