2f745e7516de534fef07b7627b3f5d5bfb4c2899
abhijit <kamathabhijit2002@gmail.com>
2024-12-22 22:32:10 -0500

    installing zustand and creating appropriate types

--- my-app/types.ts
+++ my-app/types.ts
@@ -2,4 +2,14 @@ export default interface CommitType {
   commitId: number;
   commitMessage: string;
   branchName: string;
+  stagedChange?: boolean;
+}
+
+export default interface branch {
+    branchName: string;
+    commits: CommitType[];
+    parentBranch: string;
+    fromCommit: CommitType["commitId"]; // commit id to begin 
+    toCommit?: CommitType["commitId"]; // branches can be 
+    merged: boolean;
 }
